c3125dd06f052f1bb8518238a0fa543c3c66a301
Christian-Ki <kisczio@christians-air.fritz.box>
2014-03-13 00:16:36 +0100

    Initialisierung des Sudokus ordentlich ausgelagert

---
+++ init sudoku.h
@@ -0,0 +1,14 @@
+//
+//  init sudoku.h
+//  
+//
+//  Created by Christian Kisczio on 12.03.14.
+//
+//
+
+#ifndef ____init_sudoku__
+#define ____init_sudoku__
+extern int s[9][9];
+#include <iostream>
+void init_sudoku();
+#endif /* defined(____init_sudoku__) */
--- main.cpp
+++ main.cpp
@@ -12,7 +12,83 @@
 
 #include <iostream>
 #include <string>
-#include </Users/kisczio/Desktop/Harm/Sudoku/Solve Sudoku/init sudoku.cpp>
+#include "init sudoku.h"
+
+void ausgeschlossen_Reihe(int i); //alles Vorwärtsdeklarationen; müssen noch in externe Dateien mgewandelt werden
+void ausgeschlossen_Box(int i);
+void ausgeschlossen_Spalte(int i);
+int S_count();
+void S_print_out();
+int check_einzige_Möglichkeit(int a, int b, int c, int d, int e);
+void zwei_drei_in_einer_Spalte(int a, int b);
+void zwei_drei_in_einer_Reihe(int a, int b);
+
+int s[9][9];
+
+int main() {
+    
+    init_sudoku();
+    S_print_out(); //Das Sudoku wird einmal augegeben
+    std::cout << "\n";
+    int count = S_count();//Die Anzahl der belegten Felder wird gespreichert
+    int count_b;
+    std::cout << "Anzahl der belegten Felder: " << count << "\n" << std::endl;  //und ausgegeben
+    bool wdh = true;
+    bool wdh_b = true;
+    int i, a, b, c, d;
+    do {
+        count_b = S_count();
+        do {
+            std::cout << "NÄCHSTER VERSUCH\n";
+            count = S_count();      //vor dem ersten Durchlauf wird die Anzahl der belegten Felder gezählt
+            for (i=0; i<=8; i++) { //die Reihen; Spalten und Zeilen werden nacheinander aufgerufen;jedesmal auch die check_einzige_Möglichkeit Funktion
+                std::cout << "======Teste Reihe " << i << "======\n";
+                ausgeschlossen_Reihe(i);
+                std::cout << "======Teste Spalte " << i << "=====\n";
+                ausgeschlossen_Spalte(i);
+                std::cout << "======Teste Box " << i << "=====\n";
+                ausgeschlossen_Box(i);
+                for (a=0; a<=8; a++) {
+                    for (b=0; b<=8; b++) {
+                        check_einzige_Möglichkeit(a, b, 0,0,0);
+                        
+                    }
+                }
+                
+            }
+            
+            if (count == S_count()) {       //Wenn die Zahl der belegten Felder vor dem Durchlauf der der belegten Felder nach dem Durchlauf gleicht muss der Vorgang nicht mehr wiederholt werden
+                wdh = false;
+                std::cout << "Ende ?\n";
+            }
+        }
+        while (wdh == true);
+        for(c=0; c<=8; c++) {
+            for (d=c/3; d<9 ;d+=3) {
+                zwei_drei_in_einer_Spalte(c, d);
+            }
+        }
+        for(c=0; c<=8; c++) {
+            for (d=(c/3)*3; d<=c ;d++){
+                zwei_drei_in_einer_Reihe(c, d);
+            }
+        }
+        
+        
+        
+        if (count_b == S_count()) {       //Wenn die Zahl der belegten Felder vor dem Durchlauf der der belegten Felder nach dem Durchlauf gleicht muss der Vorgang nicht mehr wiederholt werden
+            wdh_b = false;
+            std::cout << "Ende \n";
+        }
+    }
+    while(wdh_b == true);
+    
+    S_print_out(); //Das Sudoku wird ausgegeben
+    count = S_count();
+    
+    std::cout << "Anzahl der belegten Felder: " << count << std::endl; //Die Zahl der belegten Felder wird ausgegeben
+    
+}
 
 void S_print_out() {
     //Sudoku ausgeben
@@ -446,65 +522,3 @@ void zwei_drei_in_einer_Spalte(int j, int b){ //j ist die Spalte, b die Box ////
 }
 
 
-int main() {
-    S_print_out(); //Das Sudoku wird einmal augegeben
-    std::cout << "\n";
-    int count = S_count();//Die Anzahl der belegten Felder wird gespreichert
-    int count_b;
-    std::cout << "Anzahl der belegten Felder: " << count << "\n" << std::endl;  //und ausgegeben
-    bool wdh = true;
-    bool wdh_b = true;
-    int i, a, b, c, d;
-    do {
-        count_b = S_count();
-        do {
-            std::cout << "NÄCHSTER VERSUCH\n";
-            count = S_count();      //vor dem ersten Durchlauf wird die Anzahl der belegten Felder gezählt
-            for (i=0; i<=8; i++) { //die Reihen; Spalten und Zeilen werden nacheinander aufgerufen;jedesmal auch die check_einzige_Möglichkeit Funktion
-                std::cout << "======Teste Reihe " << i << "======\n";
-                ausgeschlossen_Reihe(i);
-                std::cout << "======Teste Spalte " << i << "=====\n";
-                ausgeschlossen_Spalte(i);
-                std::cout << "======Teste Box " << i << "=====\n";
-                ausgeschlossen_Box(i);
-                for (a=0; a<=8; a++) {
-                    for (b=0; b<=8; b++) {
-                        check_einzige_Möglichkeit(a, b, 0,0,0);
-                    
-                    }
-                }
-            
-            }
-        
-            if (count == S_count()) {       //Wenn die Zahl der belegten Felder vor dem Durchlauf der der belegten Felder nach dem Durchlauf gleicht muss der Vorgang nicht mehr wiederholt werden
-                wdh = false;
-                std::cout << "Ende ?\n";
-            }
-        }
-        while (wdh == true);
-        for(c=0; c<=8; c++) {
-            for (d=c/3; d<9 ;d+=3) {
-                zwei_drei_in_einer_Spalte(c, d);
-            }
-        }
-        for(c=0; c<=8; c++) {
-            for (d=(c/3)*3; d<=c ;d++){
-                zwei_drei_in_einer_Reihe(c, d);
-            }
-        }
-        
-        
-        
-        if (count_b == S_count()) {       //Wenn die Zahl der belegten Felder vor dem Durchlauf der der belegten Felder nach dem Durchlauf gleicht muss der Vorgang nicht mehr wiederholt werden
-            wdh_b = false;
-            std::cout << "Ende \n";
-        }
-    }
-    while(wdh_b == true);
-    
-    S_print_out(); //Das Sudoku wird ausgegeben
-    count = S_count();
-    
-    std::cout << "Anzahl der belegten Felder: " << count << std::endl; //Die Zahl der belegten Felder wird ausgegeben
-    
-}
